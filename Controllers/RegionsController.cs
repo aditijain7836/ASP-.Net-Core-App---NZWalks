02c95b8056cb432cf322188ae3066f764043ab57
Aditi Jain <aditijain@Aditis-MacBook-Pro.local>
2024-09-07 23:43:36 +0530

    Added Controller method to get all regions from database

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+namespace NZWalks.API;
+
+// https://localhost:1234/api/regions
+[Route("api/[controller]")]
+[ApiController]
+public class RegionsController: ControllerBase
+{
+    private readonly NZWalksDbContext dbContext;
+    public RegionsController(NZWalksDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllRegions(){
+        var regions = dbContext.Regions.ToList();
+        return Ok(regions);
+    }
+}
